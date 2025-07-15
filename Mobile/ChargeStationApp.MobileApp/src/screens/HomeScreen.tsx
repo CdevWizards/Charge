3d86833636f3a6f75453a6a4255e45028e06bec1
CdevWizards <ubahmet32@hotmail.com>
2025-07-15 23:56:54 +0300

    two commit

---
+++ Mobile/ChargeStationApp.MobileApp/src/screens/HomeScreen.tsx
@@ -0,0 +1,13 @@
+// Mobile/ChargeStationApp.MobileApp/src/screens/HomeScreen.tsx
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Hoş Geldiniz - Şarj İstasyonları</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
